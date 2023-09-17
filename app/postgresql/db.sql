c58b8f4f6b7be7a20095c747fbc87edf51fcf5d3
Sean Zhengg <s410585038@gmail.com>
2023-09-17 17:40:42 +0800

    [sql]: update column type

--- app/postgresql/db.sql
+++ app/postgresql/db.sql
@@ -3,6 +3,6 @@ create table users (
    account varchar(50) unique not null,
    password varchar(50) not null,
    nickname varchar(50) unique not null,
-   create_at timestamp,
-   update_at timestamp
+   create_at timestamp(0),
+   update_at timestamp(0)
 )
